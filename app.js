c5c3c7c7fa65b2eb412ce941062557d9cc33a3e2
= <=>
2018-12-18 00:10:35 +0900

    2nd Commit

--- app.js
+++ app.js
@@ -1,24 +1,23 @@
-var createError = require('http-errors');
 var express = require('express');
-var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var app = express();
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.listen('8080');
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-    next(createError(404));
+    res.header('Content-Type', 'application/json; charset=utf-8');
+    res.status = 404;
+    var response = { code: 9, message: 'Not Fonund' };
+    res.send(response);
 });
 // error handler
 app.use(function (err, req, res, next) {
@@ -27,7 +26,9 @@ app.use(function (err, req, res, next) {
     res.locals.error = req.app.get('env') === 'development' ? err : {};
     // render the error page
     res.status(err.status || 500);
-    res.render('error');
+    res.header('Content-Type', 'application/json; charset=utf-8');
+    var response = { code: 9, message: 'Internal Server Error!' };
+    res.send(response);
 });
 module.exports = app;
 //# sourceMappingURL=app.js.map
--- app.ts
+++ app.ts
@@ -1,6 +1,4 @@
-var createError = require('http-errors');
 var express = require('express');
-var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
@@ -10,21 +8,22 @@ var usersRouter = require('./routes/users');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+app.listen('8080')
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  res.header('Content-Type', 'application/json; charset=utf-8')
+  res.status = 404
+  var response = {code:9, message:'Not Fonund'}
+  res.send(response);
 });
 
 // error handler
@@ -35,7 +34,9 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.header('Content-Type', 'application/json; charset=utf-8')
+  var response = {code:9, message:'Internal Server Error!'}
+  res.send(response);
 });
 
 module.exports = app;
